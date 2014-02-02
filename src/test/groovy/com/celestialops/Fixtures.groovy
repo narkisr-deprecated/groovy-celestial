c30cf09502aa7c03b3db4965b91b9a5754ef6a24
narkisr <narkisr@gmail.com>
2014-02-02 13:23:06 +0200

    moving to json

--- src/test/groovy/com/celestialops/Fixtures.groovy
+++ src/test/groovy/com/celestialops/Fixtures.groovy
@@ -3,17 +3,9 @@ package com.celestialops
 import groovy.json.JsonSlurper
 
 class Fixtures {
-  def dockerSystem = 
-    [description: '', type: 'redis', owner: 'admin', env: 'dev',
-     machine: [ hostname: 'foobar', cpus: 4, memory: 512],
-     docker: [
-      node: 'local', image: 'narkisr:latest',
-      volumes: [ '/tmp:/tmp' ],
-      'exposed-ports': [ '22/tcp' ],
-      'port-bindings': [ '22/tcp:2221/0.0.0.0' ]
-     ]
-   ]
 
-   def redisType = new JsonSlurper().parse(new File('src/test/resources/redis.json'))
+  def dockerSystem =  new JsonSlurper().parse(new File('src/test/resources/docker-system.json'))
+
+  def redisType = new JsonSlurper().parse(new File('src/test/resources/redis.json'))
 
 }
